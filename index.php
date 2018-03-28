f88ee8b5c4fce62b61aa534cb8bfa205de3767fd
Gentille Plume <melvin.lemoine@feather-project.org>
2018-03-28 22:20:03 +0200

    Developed

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Stick Fight: the game</title>
+        
+        <meta charset="utf-8">
+        
+        <!--########## STYLES SHEETS ##########-->
+        
+        <!--TOOLS & RESSOURCES-->
+        <link href="#" rel="stylesheet" type="text/css">
+        
+    </head>
+
+<!--####################################################################################################-->
+    
+    <body>
+    
+        <!--########## ELEMENTS ##########-->
+        
+        <!--ELEMENT-->
+        <?php include 'elements/element.html'?>
+        
+        <!--########## SCRIPTS ##########-->
+        
+        <!--SCRIPTS-->
+        <script src="#"></script>
+        
+    </body>
+    
+</html>
